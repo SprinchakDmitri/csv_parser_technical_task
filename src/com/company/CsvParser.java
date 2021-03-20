4d6ed251ea53fcf508509f7601116b059eaf336c
sprinchak <sprinchak.dmitriy@gmail.com>
2021-03-20 18:05:25 +0200

    Added the CvsParser class. This implementation works only with fully valid files that do not contain double-quoted commas. This class provides the ability to get data from a CSV file by knowing its absolute path. It also determines what is the name of the columns and what is the data

---
+++ src/com/company/CsvParser.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.company.Main.pathToTheFile;
+
+public class CsvParser {
+
+    List<String> columnNames;
+
+    public CsvParser() throws IOException {
+        columnNames = getCsvColumnNamesFromFile(pathToTheFile);
+    }
+
+    List<String> getCsvLinesFromFile(String path) throws IOException {
+        return Files
+                .lines(Paths.get(path))
+                .skip(1)
+                .collect(Collectors.toList());
+    }
+
+    List<String> getCsvColumnNamesFromFile(String path) throws IOException {    //todo add validation for a first line
+        Optional<String> firstLine = Files.lines(Paths.get(path)).findFirst();
+        if (firstLine.isPresent()) {
+            return Arrays.asList(firstLine.get().split(","));
+        } else throw new IllegalArgumentException();
+    }
+
+    boolean validateCsvLine(String line, int columnNumber) {                    //todo implement functionality
+        return true;
+    }
+
+    List<String> splitCsvLine(String line) {                                    //todo implement split with "," case
+        if (validateCsvLine(line, columnNames.size())) {
+            return Arrays.asList(line.split(","));
+        } else return null;
+    }
+
+    List<List<String>> splitAllLines(List<String> unsplitedLines) {
+        List<List<String>> resultList = new ArrayList<>();
+        for (String line : unsplitedLines) {
+            resultList.add(splitCsvLine(line));
+        }
+        return resultList;
+    }
+
+    List<String> getLine(List<List<String>> splitedLines, int lineNumber) {
+        if (splitedLines.size() > lineNumber && lineNumber > 0)
+            return splitedLines.get(lineNumber);
+        else throw new IllegalArgumentException();
+    }
+
+    String getLineAndElement(List<List<String>> splitedLines, int lineNumber, int elementNumber) {
+        if (splitedLines.size() > lineNumber && lineNumber > 0
+                && elementNumber < columnNames.size() && elementNumber > 0)
+
+            return splitedLines.get(lineNumber)
+                    .get(elementNumber);
+        else throw new IllegalArgumentException();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
+    static String pathToTheFile = "/home/sprinchak/Downloads/convertcsv.csv";
 
     public static void main(String[] args) {
     }
